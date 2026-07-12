dbab176d0eee1b46049d9b49e201545e27621634
Ahmed Amine Abahmane <ahmed_amine.abahmane@hotmail.fr>
2026-07-12 15:34:37 +0100

    feat: implement authentication services

---
+++ backend/src/Modules/Identity/JobWize.Modules.Identity/Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JobWize.Modules.Identity.Infrastructure.Authentication
+{
+    public sealed class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+
+        [Required]
+        public string Issuer { get; init; } = default!;
+
+        [Required]
+        public string Audience { get; init; } = default!;
+
+        [Required]
+        [MinLength(32)]
+        public string SecretKey { get; init; } = default!;
+
+        [Required]
+        public TimeSpan AccessTokenLifetime { get; init; }
+
+        [Required]
+        public TimeSpan RefreshTokenLifetime { get; init; }
+    }
+}
