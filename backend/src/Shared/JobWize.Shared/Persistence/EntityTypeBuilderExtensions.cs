8c79cbd7ef97e2c3579e766a03a6aeebb850d94a
Ahmed Amine Abahmane <ahmed_amine.abahmane@hotmail.fr>
2026-07-12 15:31:23 +0100

    feat: add persistence conventions

---
+++ backend/src/Shared/JobWize.Shared/Persistence/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using JobWize.Shared.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobWize.Shared.Persistence
+{
+    public static class EntityTypeBuilderExtensions
+    {
+        public static EntityTypeBuilder<TEntity> ConfigureEntityBase<TEntity>(this EntityTypeBuilder<TEntity> builder)
+            where TEntity : Entity
+        {
+            builder.HasKey(entity => entity.Id);
+
+            return builder;
+        }
+        
+        public static EntityTypeBuilder<TEntity> ConfigureDomainModelBase<TEntity>(this EntityTypeBuilder<TEntity> builder)
+            where TEntity : DomainModel
+        {
+            builder.ConfigureEntityBase();
+
+            builder.Property(entity => entity.CreatedAt)
+                .IsRequired();
+
+            builder.Property(entity => entity.UpdatedAt);
+
+            builder.Property(entity => entity.DeletedAt);
+
+            return builder;
+        }
+    }
+}
---
+++ backend/src/Shared/JobWize.Shared/Persistence/Schemas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobWize.Shared.Persistence
+{
+    public static class Schemas
+    {
+        public const string Identity = "identity";
+    }
+}
