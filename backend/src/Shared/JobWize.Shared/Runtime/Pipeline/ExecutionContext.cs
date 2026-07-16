e0c26ff903b1cd7845364bbf6388eae9ed555aa5
Ahmed Amine Abahmane <ahmed_amine.abahmane@hotmail.fr>
2026-07-16 09:31:26 +0100

    feat(runtime): introduce pipeline abstractions

---
+++ backend/src/Shared/JobWize.Shared/Runtime/Pipeline/ExecutionContext.cs
@@ -0,0 +1,24 @@
+﻿using JobWize.Shared.Contracts.Application.Dispatching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobWize.Shared.Runtime.Pipeline
+{
+    public sealed class ExecutionContext<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        public TRequest Request { get; }
+
+        public IServiceProvider ServiceProvider { get; }
+
+        public CancellationToken CancellationToken { get; }
+
+        public ExecutionContext(TRequest request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
+        {
+            Request = request;
+            ServiceProvider = serviceProvider;
+            CancellationToken = cancellationToken;
+        }
+    }
+}
---
+++ backend/src/Shared/JobWize.Shared/Runtime/Pipeline/IPipelineBehavior.cs
@@ -0,0 +1,13 @@
+﻿using JobWize.Shared.Contracts.Application.Dispatching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobWize.Shared.Runtime.Pipeline
+{
+    public interface IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        Task<TResponse> HandleAsync(ExecutionContext<TRequest, TResponse> context, RequestExecutionDelegate<TResponse> next);
+    }
+}
